ab73993278bff7aa4a4a2c9e35dcbf9f2bf93be7
TaoTaoHub <wt.china@outlook.com>
2017-08-22 00:36:13 +0800

    insertSort

--- sort/insertSort.py
+++ sort/insertSort.py
@@ -2,12 +2,12 @@
 def insertSort(arr):
     length = len(arr)
     for i in range(1, length):
-        for j in range(i):
-            if arr[j]>arr[i]:
-                arr[i], arr[j] = arr[j], arr[i]
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
     return arr
 
 
-testlist = [17, 23, 20, 14, 12, 25, 1, 20, 81, 14, 11, 100]
+testlist = [17, 23, 20, 14, 12, 25, 1, 20, 81, 14, 11, 0]
 sortList = insertSort(testlist)
 print(sortList)
