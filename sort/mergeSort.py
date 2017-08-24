e6c35c7adba69d173d0e37dd239a7fa0749d0c53
TaoTaoHub <wt.china@outlook.com>
2017-08-24 22:30:54 +0800

    mergeSort

--- sort/mergeSort.py
+++ sort/mergeSort.py
@@ -1,7 +1,30 @@
 #归并排序
 def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    num = int(len(arr)/2)
+    left = mergeSort(arr[:num])
+    right = mergeSort(arr[num:])
+    return merge(left, right)
 
-    return arr
+def merge(left, right):
+    l = len(left)
+    r = len(right)
+    i, j = 0, 0
+    result = []
+    while i<l and j<r:
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
+
 
 testlist = [17, 23, 20, 14, 12, 25, 1, 20, 81, 14, 11, 12]
 sortList = mergeSort(testlist)
