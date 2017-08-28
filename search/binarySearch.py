80ac31dadd4fa9b6b2b2cec053c88a83b0bb236f
TaoTaoHub <wt.china@outlook.com>
2017-08-28 23:03:50 +0800

    binarySearch

---
+++ search/binarySearch.py
@@ -0,0 +1,18 @@
+#二分查找
+def binarySearch(arr, value, l, r):
+    if l >= r:
+        if arr[l] == value:
+            return l
+        else:
+            return -1
+    m = (l + r)//2
+    if arr[m] == value:
+        return m
+    elif arr[m] > value:
+        return binarySearch(arr, value, l, m - 1)
+    else:
+        return binarySearch(arr, value, m + 1, r)
+
+testlist = [1,2,4,6,7,8,9,10,22,34]
+result = binarySearch(testlist, 5, 0, len(testlist))
+print(result)
